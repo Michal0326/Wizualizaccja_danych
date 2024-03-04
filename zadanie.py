a6796ed01077bb5289ec6abef4f09653bafa6f98
lukaszracki <luki.racki@gmail.com>
2024-03-04 10:43:49 +0100

    Initial commit

---
+++ zadanie.py
@@ -0,0 +1,13 @@
+def zad1():
+    zdanie = input("Podaj zdanie: ")
+    slowa = (zdanie.split())
+    print(len(slowa))
+
+def zad2():
+    a = int(input("Podaj pierwsza liczbe: "))
+    a = int(input("Podaj druga liczbe: "))
+    a = int(input("Podaj trzecia liczbe: "))
+    
+
+zad1()
+zad2()
